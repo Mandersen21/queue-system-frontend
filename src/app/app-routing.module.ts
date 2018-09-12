6c9b2962ed66e904cb333db16412cf098c38770b
mandersen21 <mikkelandersen21@gmail.com>
2018-09-12 16:42:00 +0200

    Added updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentComponent } from 'src/app/content/content.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 const routes: Routes = [
   { path: '', component: DashboardComponent},
   { path: 'dashboard', component: DashboardComponent},
-  { path: 'content', component: ContentComponent }
+  { path: 'content', component: ContentComponent },
+  { path: 'user', component: UserProfileComponent }
 ];
 
 @NgModule({
