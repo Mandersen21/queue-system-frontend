6f65f80a8c881c22b384203c8bff87388d66afc2
mandersen21 <mikkelandersen21@gmail.com>
2018-10-07 20:53:26 +0200

    Added interval logic to automatic change between routes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,47 @@
 import { Component } from '@angular/core';
-import {trigger, animate, style, group, animateChild, query, stagger, transition} from '@angular/animations';
 import { routerTransition } from 'src/app/router.animations';
+import { interval, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [ routerTransition ],
+  animations: [routerTransition],
 })
 export class AppComponent {
   title = 'app';
 
-  constructor() {
+  currentInterval = 10000
+  router: Router;
 
+  sub: Subscription;
+
+  constructor(router: Router) {
+    this.router = router;
+
+    // Start timer
+    this.sub = interval(this.currentInterval).subscribe((val) => { this.changeIntervalBasedOnRoute() })
+  }
+
+  changeIntervalBasedOnRoute() {
+    if (this.currentInterval === 10000) {
+      this.currentInterval = 5000
+      console.log("Change interval to 5 sec")
+      this.router.navigateByUrl('/admin');
+      this.sub.unsubscribe();
+      this.sub = interval(this.currentInterval).subscribe((val) => { this.changeIntervalBasedOnRoute() })
+      return
+    }
+
+    if (this.currentInterval === 5000) {
+      this.currentInterval = 10000
+      console.log("Change interval to 10 sec")
+      this.router.navigateByUrl('/dashboard');
+      this.sub.unsubscribe();
+      this.sub = interval(this.currentInterval).subscribe((val) => { this.changeIntervalBasedOnRoute() })
+      return;
+    }
   }
 
   getState(outlet) {
