8a2fb29a3eead1e0f4cb1435aef0c853d44bc8d0
mandersen21 <mikkelandersen21@gmail.com>
2018-10-10 21:15:13 +0200

    Added changes.

--- src/app/triage/triage.component.ts
+++ src/app/triage/triage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-triage',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./triage.component.css']
 })
 export class TriageComponent implements OnInit {
+
+  @Input() fastTrack: boolean;
 
   constructor() { }
 
