0d4da2bda5b2a0978fdb35b362937c1becf2b450
mandersen21 <mikkelandersen21@gmail.com>
2018-10-27 15:59:26 +0200

    Added clock feature :)

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as moment from 'moment'; // add this 1 of 4
+
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClockComponent implements OnInit {
 
-  constructor() { }
+  public clock: any;
+  public date: any;
 
+  constructor() {
+    setInterval(() => {
+      this.clock = moment().lang("da").format('HH:mm');
+      this.date = moment().lang("da").format('LL')
+  }, 1000) // Updates the time every second.
+  }
   ngOnInit() {
+    this.clock = moment().lang("da").format('HH:mm')
+    this.date = moment().lang("da").format('LL')
   }
 
 }
