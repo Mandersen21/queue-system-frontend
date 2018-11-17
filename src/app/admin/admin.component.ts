122838bcd13ce723adf19a4b2a79d49dcea66563
mandersen21 <mikkelandersen21@gmail.com>
2018-11-17 22:41:00 +0100

    added files.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  patientRegisterModel: IPatient
+  patientUpdateModel: IPatient
+
   constructor() { }
 
   ngOnInit() {
+    this.patientRegisterModel = { name: '', infant: 0, triage: 0, queueType: 0 }
   }
 
+  private registerPatient() {
+    console.log(this.patientRegisterModel)
+  }
+
+}
+
+export interface IPatient {
+  name: string,
+  infant: boolean,
+  triage: number,
+  queueType: boolean
 }
