948561f6cc1e7bb9cff78533b10e7c806037e2b7
mandersen21 <mikkelandersen21@gmail.com>
2018-11-18 02:45:29 +0100

    Added admin page functions.

---
+++ src/app/admin/admin-service.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  public addPatient(name, infant, triage, queueType) {
+    return this.http.post(environment.backend + '/api/patients',
+      {
+        "name": name,
+        "age": infant == true ? 2 : 50,
+        "triage": triage,
+        "fastTrack": queueType
+      })
+  }
+
+  public updatePatient(patientId, name, infant, triage, queueType) {
+    return this.http.put(environment.backend + '/api/patients/' + patientId, 
+    {
+      "name": name,
+      "age": infant == true ? 2 : 50,
+      "triage": triage,
+      "fastTrack": queueType
+    })
+  }
+
+  public getPatient(patientId) {
+    return this.http.get(environment.backend + '/api/patients/' + patientId)
+  }
+}
--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AdminServiceService } from './admin-service.service';
 
 @Component({
   selector: 'app-admin',
@@ -10,22 +11,69 @@ export class AdminComponent implements OnInit {
 
   patientRegisterModel: IPatient
   patientUpdateModel: IPatient
+  patientDataFound: any
 
-  constructor() { }
+  constructor(private adminService: AdminServiceService) {
+    this.patientUpdateModel = { name: 'Mads Wehlast', infant: "true", triage: "1", queueType: "true" }
+  }
 
   ngOnInit() {
-    this.patientRegisterModel = { name: '', infant: false, triage: 0, queueType: false }
+    this.patientRegisterModel = { name: '', infant: "false", triage: "4", queueType: "false" }
+    this.patientUpdateModel = { name: '', infant: "true", triage: "1", queueType: "true" }
   }
 
   private registerPatient() {
-    console.log(this.patientRegisterModel)
+    console.log("Register patient", this.patientRegisterModel)
+    if (this.patientRegisterModel.name.length > 0) {
+      let name = this.patientRegisterModel.name
+      let infant = this.patientRegisterModel.infant == "false" ? false : true
+      let triage = Number(this.patientRegisterModel.triage)
+      let queueType = this.patientRegisterModel.queueType == "false" ? false : true
+      this.adminService.addPatient(name, infant, triage, queueType).subscribe(
+        data => { console.log("POST Request is successful ", data); },
+        error => { console.log("Error", error); }
+      );
+    }
+  }
+
+  private updatePatient() {
+    console.log("Update patient", this.patientUpdateModel)
+    if (this.patientUpdateModel.patientId.length > 0 && this.patientUpdateModel.name.length > 0) {
+      let patientId = this.patientUpdateModel.patientId
+      let name = this.patientUpdateModel.name
+      let infant = this.patientUpdateModel.infant == "false" ? false : true
+      let triage = Number(this.patientUpdateModel.triage)
+      let queueType = this.patientUpdateModel.queueType == "false" ? false : true
+      this.adminService.updatePatient(patientId, name, infant, triage, queueType).subscribe(
+        data => { console.log("Update Request is successful ", data); },
+        error => { console.log("Error", error); }
+      );
+    }
+  }
+  
+  private getPatient() {
+    console.log("Getting patient info")
+    console.log(this.patientUpdateModel.patientId)
+    if (this.patientUpdateModel.patientId.length > 0) {
+      let patientId = this.patientUpdateModel.patientId
+      this.adminService.getPatient(patientId).subscribe(
+        data => { console.log("Patient found "), this.patientDataFound = data },
+        error => { console.log("Error", error) },
+        () => this.updateValues()
+      )
+    }
+  }
+
+  private updateValues() {
+    console.log(this.patientDataFound)
   }
 
 }
 
 export interface IPatient {
+  patientId?: string,
   name: string,
-  infant: boolean,
-  triage: number,
-  queueType: boolean
+  infant: string,
+  triage: string,
+  queueType: string
 }
