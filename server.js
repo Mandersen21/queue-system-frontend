44d853c6ee05b1b987a25555855b44ac5ab1279c
mandersen21 <mikkelandersen21@gmail.com>
2018-11-21 13:03:26 +0100

    Added mistake with project name.

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/queue-system-frontend'));
+app.use(express.static(__dirname + '/dist/angular-project'));
 
-app.get('/*all', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/queue-system-frontend/index.html'));
-  });
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/angular-project/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
+console.log("listening on port: ", process.env.PORT || 8080)
 app.listen(process.env.PORT || 8080);
